6d0b55ad1e1dc3d15152bbbbb94c187cd2afc713
mcgidoug <mcgidoug@gmail.com>
2022-10-11 12:21:47 -0500

    added event listeners and start function

--- index.js
+++ index.js
@@ -18,3 +18,71 @@ const bottomRight = document.querySelector("#bottomright"); // blue button
 const strictButton = document.querySelector("#strict"); // strict mode button
 const onButton = document.querySelector("#on"); // on button
 const startButton = document.querySelector("#start"); // start button
+
+// checkboxes accepts booleans
+strictButton.addEventListener("click", (event) => {
+  if (strictButton.checked == true) {
+    strict = true;
+  } else {
+    strict = false;
+  }
+});
+
+onButton.addEventListener("click", (event) => {
+  if (onButton.checked == true) {
+    on = true;
+    turnCounter.innerHTML = ":)";
+  } else {
+    on = false;
+    turnCounter.innerHTML = "";
+    clearColor(); // clear simon buttons when OFF
+    clearInterval(intervalId);
+  }
+});
+
+startButton.addEventListener("click", (event) => {
+  if (on || win) {
+    play();
+  }
+});
+
+function play() {
+  win = false;
+  order = [];
+  playerOrder = [];
+  flash = 0;
+  intervalId = 0;
+  turn = 1;
+  turnCounter.innerHTML = 1;
+  good = true;
+  //   comp chooses random number for color for 20 turns
+  for (let i = 0; i < 20; i++) {
+    order.push(Math.floor(Math.random() * 4) + 1);
+  }
+  compTurn = true;
+
+  intervalId = setInterval(gameTurn, 800); // computer flashes light every 800 miliseconds
+}
+
+function gameTurn() {
+  on = false; // does not allow player to click buttons while computer is flashing
+
+  //   if number of times lights have flashed = turn number we're on THEN comp turn over
+  if (flash == turn) {
+    clearInterval(intervalId);
+    compTurn = false;
+    clearColor();
+    on = true; // player can start pressing buttons
+  }
+
+  if (compTurn) {
+    clearColor();
+    setTimeout(() => {
+      if (order[flash] == 1) one();
+      if (order[flash] == 2) two();
+      if (order[flash] == 3) three();
+      if (order[flash] == 4) four();
+      flash++;
+    }, 200);
+  }
+}
